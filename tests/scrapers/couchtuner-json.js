920b669f5c2c98a9f482896216565fe7493cba5b
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-11-27 20:34:44 -0500

    Pulls test answers into own file
    Keep it clean

---
+++ tests/scrapers/couchtuner-json.js
@@ -0,0 +1,140 @@
+module.exports = {
+  'scrapeTV':{
+    numOfListings:603,
+    firstListing: {
+      name: '11.22.63',
+      link: 'http://www.couch-tuner.ag/watch-11-22-63-online/'
+    }
+  }, 'scrapeEpisodes': {
+    numOfEpisodes:91,
+    numOfMissed:2,
+    firstEpisode: {
+      link: 'http://www.couch-tuner.ag/2016/11/the-walking-dead-season-7-episode-5-go-getters/',
+      season: '7',
+      episode: '5'
+    }
+  }, 'watchIt': {
+    videoLink:'http://couch-tuner.city/5/the-walking-dead-s06-greeting-from-the-set-of-season/',
+    videoLinkPreresolved:'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e5'
+  }, 'scrapeEpisodeID': {
+    videoLinks: [ 'br03hat1bewy' ]
+  }, 'lastFive': {
+    listings:
+      [ { name: 'Your Family Or Mine',
+          link: 'http://www.couch-tuner.ag/watch-your-family-or-mine-online/' },
+        { name: 'Your Pretty Face Is Going to Hell',
+          link: 'http://www.couch-tuner.ag/watch-your-pretty-face-is-going-to-hell-online/' },
+        { name: 'You’re The Worst',
+          link: 'http://www.couch-tuner.ag/watch-youre-the-worst-online-1/' },
+        { name: 'Zoe Ever After',
+          link: 'http://www.couch-tuner.ag/watch-zoe-ever-after-online/' },
+        { name: 'Zoo 2015',
+          link: 'http://www.couch-tuner.ag/watch-zoo-2015-online/' } ],
+    shows: {
+      'Your Family Or Mine': {
+        episodes:[ { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-7-5-stages/',
+                     season: '1',
+                     episode: '7' },
+                   { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-6-the-speech/',
+                     season: '1',
+                     episode: '6' },
+                   { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-5-christmas-in-july/',
+                     season: '1',
+                     episode: '5' },
+                   { link: 'http://www.couch-tuner.ag/2015/04/your-family-or-mine-season-1-episode-4-the-will/',
+                     season: '1',
+                     episode: '4' },
+                   { link: 'http://www.couch-tuner.ag/2015/04/your-family-or-mine-season-1-episode-1-pilot/',
+                     season: '1',
+                     episode: '1' } ],
+        missed:[]
+      },'Your Pretty Face Is Going to Hell': {
+        episodes:[ { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e5/',
+                     season: '1',
+                     episode: '5' },
+                   { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e4/',
+                     season: '1',
+                     episode: '4' },
+                   { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e3/',
+                     season: '1',
+                     episode: '3' },
+                   { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e2/',
+                     season: '1',
+                     episode: '2' },
+                   { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e1/',
+                     season: '1',
+                     episode: '1' } ],
+        missed:[ { name: 'Your Pretty Face Is Going To Hell Season 3 Episode&nbsp;4',
+                   link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s3e4/' },
+                 { name: 'Your Pretty Face Is Going To Hell Season 3 Episode&nbsp;3',
+                   link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s3e3/' } ]
+      }, 'You’re The Worst': {
+        episodes:[ { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-5-sunday-funday/',
+                     season: '1',
+                     episode: '5' },
+                   { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-4-what-normal-people-do/',
+                     season: '1',
+                     episode: '4' },
+                   { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-3-keys-open-doors/',
+                     season: '1',
+                     episode: '3' },
+                   { link: 'http://www.couch-tuner.ag/2014/07/youre-the-worst-season-1-episode-2-insouciance/',
+                     season: '1',
+                     episode: '2' },
+                   { link: 'http://www.couch-tuner.ag/2014/07/youre-the-worst-season-1-episode-1-pilot/',
+                     season: '1',
+                     episode: '1' } ],
+        missed:[ { name: 'You',
+                   link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-13-no-longer-just-us/' },
+                 { name: '‘',
+                   link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
+                 { name: 'You',
+                   link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-12-you-knew-it-was-a-snake/' },
+                 { name: '‘',
+                   link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
+                 { name: 'You',
+                   link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-10-talking-to-me-talking-to-me/' },
+                 { name: '‘',
+                   link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
+                 { name: 'You',
+                   link: 'http://www.couch-tuner.ag/2016/09/youre-the-worst-season-3-episode-5-twenty-two/' },
+                 { name: '’',
+                  link: 'http://www.couch-tuner.ag/2016/09/youre-the-worst-season-3-episode-4-men-get-strong/' } ]
+      }, 'Zoe Ever After': {
+        episodes: [ { link: 'http://www.couch-tuner.ag/2016/02/zoe-ever-after-season-1-episode-5-ghost/',
+                      season: '1',
+                      episode: '5' },
+                    { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-4-zoe-ever-after/',
+                      season: '1',
+                      episode: '4' },
+                    { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-3-zoe-ever-after/',
+                      season: '1',
+                      episode: '3' },
+                    { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-2-the-third-wheel-gets-slapped/',
+                      season: '1',
+                      episode: '2' },
+                    { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-1-pilot/',
+                      season: '1',
+                      episode: '1' } ],
+        missed:[]
+      }, 'Zoo 2015': {
+        episodes: [ { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-5-blame-it-on-leo/',
+                      season: '1',
+                      episode: '5' },
+                    { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-4-pack-mentality/',
+                      season: '1',
+                      episode: '4' },
+                    { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-3-the-silence-of-the-cicadas/',
+                      season: '1',
+                      episode: '3' },
+                    { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-2-fight-or-flight/',
+                      season: '1',
+                      episode: '2' },
+                    { link: 'http://couch-tuner.city/5/zoo-s1-e1-first-blood/',
+                      season: '1',
+                      episode: '1' } ],
+        missed:[]
+      }
+    }
+  }
+};
--- tests/scrapers/couchtuner.js
+++ tests/scrapers/couchtuner.js
@@ -1,74 +1,72 @@
 let tape = require('tape');
 let Couchtuner = require('../../scrapers/couchtuner');
+let answers = require('./couchtuner-json');
 let couch = new Couchtuner();
 
-tape.skip('TV', (t) => {
+tape('scrapeTV', (t) => {
   couch.scrapeTV('http://www.couch-tuner.ag/tv-lists').then((anchors) => {
     t.equal(
       anchors.length,
-      603,
+      answers.scrapeTV.numOfListings,
       'Number of tv listings'
     );
 
     t.deepEqual(
       anchors[0],
-      { name: '11.22.63',
-        link: 'http://www.couch-tuner.ag/watch-11-22-63-online/' },
+      answers.scrapeTV.firstListing,
       'First listing returned by couchtuner'
     );
   }).catch(t.fail).then(t.end);
 });
 
-tape.skip('Episodes', (t) => {
+tape('scrapeEpisodes', (t) => {
   couch.scrapeEpisodes('http://www.couch-tuner.ag/watch-the-walking-dead-online-streamin').then((result) => {
     t.equal(
       result.episodes.length,
-      91,
+      answers.scrapeEpisodes.numOfEpisodes,
       'Number of episodes'
     );
 
     t.equal(
       result.missed.length,
-      2,
+      answers.scrapeEpisodes.numOfMissed,
       'Number of mismatched links'
     );
 
     t.deepEqual(
       result.episodes[0],
-      { link: 'http://www.couch-tuner.ag/2016/11/the-walking-dead-season-7-episode-5-go-getters/',
-        season: '7',
-        episode: '5' },
+      answers.scrapeEpisodes.firstEpisode,
       'First episode'
     );
   }).catch(t.fail).then(t.end);
 });
 
-tape.skip('Watch It', (t) => {
+tape('scrapeWatchIt', (t) => {
   couch.scrapeWatchIt('http://www.couch-tuner.ag/2015/09/the-walking-dead-s06-greeting-from-the-set-of-season').then((result) => {
     t.equal(
       result,
-      'http://couch-tuner.city/5/the-walking-dead-s06-greeting-from-the-set-of-season/',
+      answers.watchIt.videoLink,
       'Link to page with video'
     );
   }).catch(t.fail).then(t.end);
 });
 
-tape.skip('Watch It - Preresolved', (t) => {
+tape('scrapeWatchIt - Preresolved', (t) => {
   //Sometimes `scrapeEpisodes` returns a watchit link
   couch.scrapeWatchIt('http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e5').then((result) => {
     t.equal(
       result,
-      'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e5',
+      answers.watchIt.videoLinkPreresolved,
       'Returns link if it\'s from `.city` tld'
     );
   }).catch(t.fail).then(t.end);
 });
 
-tape.skip('Episode Link', (t) => {
+tape('scrapeEpisodeID', (t) => {
   couch.scrapeEpisodeID('http://couch-tuner.city/5/the-walking-dead-s2-e7-pretty-much-dead-already/').then((result) => {
     t.deepEqual(
       result,
-      [ 'br03hat1bewy' ],
+      answers.scrapeEpisodeID.videoLinks,
       'Returns all embded video links'
     );
   }).catch(t.fail).then(t.end);
@@ -80,16 +78,7 @@ tape('Last Five', (t) => {
 
     t.deepEqual(
       listings,
-      [ { name: 'Your Family Or Mine',
-          link: 'http://www.couch-tuner.ag/watch-your-family-or-mine-online/' },
-        { name: 'Your Pretty Face Is Going to Hell',
-          link: 'http://www.couch-tuner.ag/watch-your-pretty-face-is-going-to-hell-online/' },
-        { name: 'You’re The Worst',
-          link: 'http://www.couch-tuner.ag/watch-youre-the-worst-online-1/' },
-        { name: 'Zoe Ever After',
-          link: 'http://www.couch-tuner.ag/watch-zoe-ever-after-online/' },
-        { name: 'Zoo 2015',
-          link: 'http://www.couch-tuner.ag/watch-zoo-2015-online/' } ],
+      answers.lastFive.listings,
       'Last five listings'
     );
 
@@ -113,111 +102,7 @@ tape('Last Five', (t) => {
 
     t.deepEqual(
       shows,
-      { 'Your Family Or Mine': {
-          episodes:[ { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-7-5-stages/',
-                       season: '1',
-                       episode: '7' },
-                     { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-6-the-speech/',
-                       season: '1',
-                       episode: '6' },
-                     { link: 'http://www.couch-tuner.ag/2015/05/your-family-or-mine-season-1-episode-5-christmas-in-july/',
-                       season: '1',
-                       episode: '5' },
-                     { link: 'http://www.couch-tuner.ag/2015/04/your-family-or-mine-season-1-episode-4-the-will/',
-                       season: '1',
-                       episode: '4' },
-                     { link: 'http://www.couch-tuner.ag/2015/04/your-family-or-mine-season-1-episode-1-pilot/',
-                       season: '1',
-                       episode: '1' } ],
-          missed:[]
-        },'Your Pretty Face Is Going to Hell': {
-          episodes:[ { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e5/',
-                       season: '1',
-                       episode: '5' },
-                     { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e4/',
-                       season: '1',
-                       episode: '4' },
-                     { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e3/',
-                       season: '1',
-                       episode: '3' },
-                     { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e2/',
-                       season: '1',
-                       episode: '2' },
-                     { link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s1e1/',
-                       season: '1',
-                       episode: '1' } ],
-          missed:[ { name: 'Your Pretty Face Is Going To Hell Season 3 Episode&nbsp;4',
-                     link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s3e4/' },
-                   { name: 'Your Pretty Face Is Going To Hell Season 3 Episode&nbsp;3',
-                     link: 'http://couch-tuner.city/5/your-pretty-face-is-going-to-hell-s3e3/' } ]
-        }, 'You’re The Worst': {
-          episodes:[ { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-5-sunday-funday/',
-                       season: '1',
-                       episode: '5' },
-                     { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-4-what-normal-people-do/',
-                       season: '1',
-                       episode: '4' },
-                     { link: 'http://www.couch-tuner.ag/2014/08/youre-the-worst-season-1-episode-3-keys-open-doors/',
-                       season: '1',
-                       episode: '3' },
-                     { link: 'http://www.couch-tuner.ag/2014/07/youre-the-worst-season-1-episode-2-insouciance/',
-                       season: '1',
-                       episode: '2' },
-                     { link: 'http://www.couch-tuner.ag/2014/07/youre-the-worst-season-1-episode-1-pilot/',
-                       season: '1',
-                       episode: '1' } ],
-          missed:[ { name: 'You',
-                     link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-13-no-longer-just-us/' },
-                   { name: '‘',
-                     link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
-                   { name: 'You',
-                     link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-12-you-knew-it-was-a-snake/' },
-                   { name: '‘',
-                     link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
-                   { name: 'You',
-                     link: 'http://www.couch-tuner.ag/2016/11/youre-the-worst-season-3-episode-10-talking-to-me-talking-to-me/' },
-                   { name: '‘',
-                     link: 'http://www.couch-tuner.ag/2016/10/youre-the-worst-season-3-episode-9-the-seventh-layer/' },
-                   { name: 'You',
-                     link: 'http://www.couch-tuner.ag/2016/09/youre-the-worst-season-3-episode-5-twenty-two/' },
-                   { name: '’',
-                    link: 'http://www.couch-tuner.ag/2016/09/youre-the-worst-season-3-episode-4-men-get-strong/' } ]
-        }, 'Zoe Ever After': {
-          episodes: [ { link: 'http://www.couch-tuner.ag/2016/02/zoe-ever-after-season-1-episode-5-ghost/',
-                        season: '1',
-                        episode: '5' },
-                      { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-4-zoe-ever-after/',
-                        season: '1',
-                        episode: '4' },
-                      { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-3-zoe-ever-after/',
-                        season: '1',
-                        episode: '3' },
-                      { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-2-the-third-wheel-gets-slapped/',
-                        season: '1',
-                        episode: '2' },
-                      { link: 'http://www.couch-tuner.ag/2016/01/zoe-ever-after-season-1-episode-1-pilot/',
-                        season: '1',
-                        episode: '1' } ],
-          missed:[]
-        }, 'Zoo 2015': {
-          episodes: [ { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-5-blame-it-on-leo/',
-                        season: '1',
-                        episode: '5' },
-                      { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-4-pack-mentality/',
-                        season: '1',
-                        episode: '4' },
-                      { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-3-the-silence-of-the-cicadas/',
-                        season: '1',
-                        episode: '3' },
-                      { link: 'http://www.couch-tuner.ag/2015/07/zoo-season-1-episode-2-fight-or-flight/',
-                        season: '1',
-                        episode: '2' },
-                      { link: 'http://couch-tuner.city/5/zoo-s1-e1-first-blood/',
-                        season: '1',
-                        episode: '1' } ],
-          missed:[]
-        }
-      }
+      answers.lastFive.shows
     );
   }).then((shows) => {
     var watchits = Promise.resolve({});
