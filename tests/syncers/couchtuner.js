1cb1db302d6d689a6ef8552f321d3562b9c72c9e
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-12-09 01:59:01 -0500

    Syncs couchtuner listings

--- tests/syncers/couchtuner.js
+++ tests/syncers/couchtuner.js
@@ -1,8 +1,20 @@
+let pg = require('pg');
 let tape = require('tape');
 let Couchtuner = require('../../syncers/couchtuner');
-let answers = require('./couchtuner-json');
-let couch = new Couchtuner();
 
-couch.syncListings().then((result) => {
-  console.log(result);
+let pool = new pg.Pool({
+  user: 'alfred',
+  password: 'password123',
+  database: 'libertytv',
+  host: 'localhost',
+  port: 5432,
+  max: 2,
+  idleTimeoutMillis: 30000,
+});
+let couch = new Couchtuner(pool);
+
+tape('syncListings', (t) => {
+  couch.syncListings().then((result) => {
+    console.log(result);
+  }).catch(t.fail).then(t.end);
 });
